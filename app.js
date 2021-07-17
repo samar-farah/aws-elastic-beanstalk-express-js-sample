67dba6b43c213f0a6763ef4befb15327b07fbf26
samar-farah <cerpamaryu@gmail.com>
2021-07-17 14:27:25 -0500

    Actualizando Hola mundo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola, Chelsy. Hice esto con amor para ti. Casi se me olvida; Te QUIERO.'));
+app.get('/', (req, res) => res.send('Hola mundo! Este es mi primier pipeline con AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
