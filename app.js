c52736b4e6c95514a539b62d536a0395a47c18f0
samar-farah <cerpamaryu@gmail.com>
2021-07-17 15:18:55 -0500

    Prueba pipeline ok

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola mundo! Este es mi primer pipeline con AWS. Provando pipeline'));
+app.get('/', (req, res) => res.send('Hola mundo! Este es mi primer pipeline con AWS. Probando pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
